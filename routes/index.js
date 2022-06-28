0639d8107da4c0b466e011d95bcd71d199dcfa46
Гаас Андрей Александрович <Gaas@gpntbsib.ru>
2022-06-28 20:44:14 +0700

    biography page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ const { Router } = require('express');
 
 const router = new Router();
 
-router.get('/', require('./home'));
+router.get('/', (req, res) => {
+  res.render('home');
+});
+
+router.get('/biography', (req, res) => {
+  res.render('biography');
+});
 
 module.exports = router;
