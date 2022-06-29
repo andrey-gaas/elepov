6ee9c2cbca07d386dd23b7038cac465dc8e74fdf
Гаас Андрей Александрович <Gaas@gpntbsib.ru>
2022-06-29 18:24:12 +0700

    editing data

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,35 +1,48 @@
+function editData(field, inncorrectMessage) {
+  return function() {
+    const input = field.childNodes[1];
+    const editButton = field.childNodes[2];
+
+    
+    input.disabled = false;
+    editButton.classList.add('save');
+
+    editButton.onclick = function(event) {
+      const value = input.value;
+      if (!value.length) {
+        alert(inncorrectMessage);
+      } else {
+        editButton.onclick = null;
+        fetch('/profile', {
+          method: 'put',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ [input.name]: value }),
+        })
+          .then(response => response.json())
+          .then(response => {
+            editButton.classList.remove('save');
+            if (response.success) {
+              input.disabled = true;
+              editButton.onclick = editData(field, inncorrectMessage);
+            }
+          })
+          .catch(error => {
+            console.log(error);
+            alert(error)
+          });
+      }
+    }
+  };
+}
+
 // Редактирование ФИО
 const nameField = document.querySelector('#name-field');
-const nameInput = nameField.childNodes[1];
 const nameEditButton = nameField.childNodes[2];
+nameEditButton.onclick = editData(nameField, 'Введите корректное ФИО');
 
-
-nameEditButton.onclick = function() {
-  nameInput.disabled = false;
-
-  nameEditButton.onclick = function() {
-    const value = nameInput.value;
-    if (!value.length) {
-      alert('Введите корректное ФИО');
-    } else {
-      nameEditButton.onclick = null;
-      fetch('/profile', {
-        method: 'put',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name: value }),
-      })
-        .then(response => response.json())
-        .then(response => {
-          if (response.success) {
-            nameInput.disabled = true;
-          }
-        })
-        .catch(error => {
-          console.log(error);
-          alert(error)
-        });
-    }
-  }
-}
+// Редактирование организации
+const organizationField = document.querySelector('#organization-field');
+const organizationEditButton = organizationField.childNodes[2];
+organizationEditButton.onclick = editData(organizationField, 'Введите корректное название организации');
